10ebfdf8ff1bf56608754bc9e11d5124ddf9f189
Jibriel Ustarz <jaustarz@cpp.edu>
2021-09-24 20:17:50 -0700

    added the code

--- numberguessinggame.c
+++ numberguessinggame.c
@@ -5,3 +5,70 @@
 /*Make an if statement for changing the max value, and the max value can't be more than 10 and less than 1*/
 /*If you press 3 the game just ends*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int max_value = 10;
+    time_t t;
+    srand((unsigned) time(&t));
+    int randomnumber = rand() %  max_value + 1;
+
+    int input;
+    while(input != 3)
+    {
+        printf("Press 1 to play a game\n");
+        printf("Press 2 to change the max number\n");
+        printf("Press 3 to quit\n");
+        scanf("%d", &input);
+        if(input == 1)
+        {
+            printf("Okay! Let's play the game!\n");
+            printf("Pick a number between 1 and 10, and let's see if you're right!\n");
+            int input2;
+            scanf("%d", &input2);
+            if(input2 == randomnumber)
+            {
+                printf("Congratulations! You got it right!\n");
+                printf("Wanna play again?\n");
+            }
+            else if(input2 > randomnumber)
+            {
+                printf("Unfortunately, that is too high and isn't correct.\n");
+                printf("Wanna try again?\n");
+            }
+            else if(input2 < randomnumber)
+            {
+                printf("Unfortunately, that is too low and isn't correct.\n");
+                printf("Wanna try again?\n");
+            }
+
+        }
+        else if(input == 2)
+        {
+            printf("You want to change the max value? Okay! Just know it can't be above 10 and less than 1!\n");
+            int input3;
+            scanf("%d", &input3);
+            if(input3 > 10)
+            {
+                printf("You can't do that.\n");
+            }
+            else if(input3 < 1)
+            {
+                printf("You can't do that.\n");
+            }
+            else
+            {
+                max_value = input3;
+                printf("Max Value has been changed to %d.\n", input3);
+            }
+
+        }
+        else if(input == 3)
+        {
+            printf("Thanks for playing! Bye!\n");
+        }
+    }
+}
