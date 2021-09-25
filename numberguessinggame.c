b374c0cd639802b933217b38dd6aeeeb269da420
Jibriel Ustarz <jaustarz@cpp.edu>
2021-09-24 20:16:25 -0700

    Created the file and added the outline

---
+++ numberguessinggame.c
@@ -0,0 +1,7 @@
+/*For this program I will insert a random number generator that picks between 1 and 10*/
+/*Then I will use a while loop to create the game. I'll create the menu in the loop*/
+/*Then I will put an if statement in the while loop to get the game working*/
+/*Use scanf for the initial input, and if said input is more or less than the random number, you lose the game*/
+/*Make an if statement for changing the max value, and the max value can't be more than 10 and less than 1*/
+/*If you press 3 the game just ends*/
+
