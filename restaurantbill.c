f1722c30ce484c432fe01f80de8c0f74fd9c298a
Jibriel Ustarz <jaustarz@cpp.edu>
2021-09-24 18:42:56 -0700

    Made the file, added the outline

---
+++ restaurantbill.c
@@ -0,0 +1,8 @@
+/*How I'm going to do this is by making the meal cost an array*/
+/*Then I'm going to list all the other variables*/
+/*I'll use scanf to ask for the tip and tax percentage*/
+/*I included a random number generator that generated the numbers 1-4*/
+/*The number corresponds to the array number of the meal cost, so it will display one of the meal costs*/
+/*It goes through a for loop and then the random number will equal the array number*/
+/*Then it calculates the tax cost and tip cost and adds those numbers to the total bill and then prints the total bill*/
+
